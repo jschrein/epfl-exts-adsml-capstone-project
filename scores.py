073af8ee02239d0956a16e942c4eccab50ebf7d4
Jörg Schreiner <joerg.schreiner@gmail.com>
2020-12-08 00:27:08 +0100

    Save the model scores. Inverty y-axis for validation curves.

---
+++ scores.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+
+
+def save_score(model, mae, r2, csv_path):
+    """Add or update model accuracy in the file given by csv_path."""
+    cols = ["mae", "r2"]
+    if os.path.exists(csv_path):
+        df = pd.read_csv(csv_path, index_col=0)
+    else:
+        df = pd.DataFrame(columns=cols)
+
+    if model in df.index:
+        df.loc[model][cols] = [mae, r2]
+    else:
+        df1 = pd.DataFrame.from_dict({model: [mae, r2]}, orient="index", columns=cols)
+        df = df.append(df1)
+
+    df.index.name = "model"
+    df.to_csv(csv_path)
